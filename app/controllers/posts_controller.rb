8fdeabf4a10c9e926cd3d1e450ab3658ac5a9ab7
jfchen19 <jchenf19@gmail.com>
2020-12-24 19:26:34 +0800

    11/23_5_done

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,31 @@
 class PostsController < ApplicationController
+  before_action :session_required, only: [:create]
+  before_action :set_board, only: [:new, :create]
+
   def new
+    @post = Post.new
+    # @board = Board.find(params:[:board_id])
+  end
+
+  def create
+    # @board = Board.find(params:[:board_id])
+    @post = Post.new(post_params)
+    @post.board = @board
+    @post.user = current_user
+
+    if @post.save
+      redirect_to root_path, notice: '新增文章成功'
+    else
+      render :new
+    end
+  end
+
+  private
+  def set_board
+    @board = Board.find(params:[:board_id])
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :content)
   end
 end
--- app/models/board.rb
+++ app/models/board.rb
@@ -2,4 +2,6 @@ class Board < ApplicationRecord
 
   validates :title, presence: true, length: { minimum: 4 }
 
+  has_many :posts
+
 end
