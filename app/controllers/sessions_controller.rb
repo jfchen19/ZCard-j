9a7650ba94f4c0db67f30a02ad27f7d10143ec4b
jfchen19 <jchenf19@gmail.com>
2020-12-24 12:09:59 +0800

    11/23_1_done

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,11 +9,12 @@ class SessionsController < ApplicationController
       session[:user9527] = params[:user][:email]  # 以email作為號碼牌 Server:session/User:cookie
       redirect_to root_path, notice: '登入成功'
     else
-      redirect_to session_path, notice: '登入失敗'
+      redirect_to sign_in_users_path, notice: '登入失敗'
     end
-    def destroy
-      session[:user9527] = nil
-      redirect_to root_path, notice: '已登出'
-    end
+  end
+
+  def destroy
+    session[:user9527] = nil
+    redirect_to root_path, notice: '已登出'
   end
 end
